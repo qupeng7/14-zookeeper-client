1f82ae9d8fbed7b2e24beb824bce205f960b1daf
qupeng <qupeng7@126.com>
2019-09-02 22:44:34 +0800

    commit

---
+++ src/main/java/com/qupeng/Test.java
@@ -0,0 +1,80 @@
+package com.qupeng;
+
+import com.qupeng.service.GoodsService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class Test {
+
+	//倒计算器
+	CountDownLatch countDownLatch = new CountDownLatch(1);
+
+	public static void main(String[] args) {
+		Test test = new Test();
+		test.runThread();
+	}
+
+	/**
+	 * 多线程并发执行
+	 */
+	private  void runThread() {
+		//spring容器
+		ApplicationContext context = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
+		GoodsService goodsService = context.getBean("goodsServiceImpl", GoodsService.class);
+
+		//等待时间
+		final long awaitTime = 5 * 1000;
+
+		//创建一个确定的线程池
+		ExecutorService executorService = Executors.newFixedThreadPool(16);
+		for (int i=0; i<16; i++) {
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+
+			//提交线程到线程池去执行
+			executorService.submit(new Runnable() {
+				@Override
+				public void run() {
+					try {
+						//等待，线程就位，但是不运行
+						countDownLatch.await();
+						System.out.println("Thread:"+Thread.currentThread().getName() + ", time: "+System.currentTimeMillis());
+
+						//执行业务代码 (超卖测试)
+						try {
+							goodsService.updateByPrimaryKeyStore(1);
+
+						} catch (Throwable e) {
+							System.out.println("Thread:"+Thread.currentThread().getName() + e.getMessage());
+						}
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+			});
+		}
+		//倒计算器 -1，那么16个线程就同时开始执行，那么就达到并发效果
+		countDownLatch.countDown();
+		try {
+			// 传达完毕信号
+			executorService.shutdown();
+			// (所有的任务都结束的时候，返回TRUE)
+			if(!executorService.awaitTermination(awaitTime, TimeUnit.MILLISECONDS)){
+				// 超时的时候向线程池中所有的线程发出中断(interrupted)。
+				executorService.shutdownNow();
+			}
+		} catch (InterruptedException e) {
+			// awaitTermination方法被中断的时候也中止线程池中全部的线程的执行。
+			System.out.println("awaitTermination interrupted: " + e);
+			executorService.shutdownNow();
+		}
+	}
+}
